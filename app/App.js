d9848b0f4a3caa2034f694f33f1dc339414fa714
UsPortatil <jonathariflo@gmail.com>
2025-11-05 17:13:50 -0600

    view response back

--- app/App.js
+++ app/App.js
@@ -1,10 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [data, setData] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('https://server-heald.onrender.com/');
+      const json = await response.json();
+      const jsonString = JSON.stringify(json);
+      setData(jsonString);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Press the button to fetch data!</Text>
+      <Button title="Fetch Data" onPress={fetchData} />
+      {data && <Text>Fetched data: {data}</Text>}
       <StatusBar style="auto" />
     </View>
   );
