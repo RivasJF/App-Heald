027e9f7a5b3ab09f6462704fa81eb08528b4c5e0
UsPortatil <jonathariflo@gmail.com>
2025-11-21 21:05:38 -0600

    feuture: opcion para rol y login

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
+import Constants from 'expo-constants';
 
 const AuthContext = createContext();
 
@@ -17,11 +18,21 @@ export function AuthProvider({ children }) {
         // const data = await SecureStore.getItemAsync("user");
         // const userToSet = data ? JSON.parse(data) : null;
 
-        // DEMO: cliente
-        const userToSet = { id: 1, role: "cliente" };
-
-        // DEMO: doctor
-        //const userToSet = { id: 1, role: "doctor" };
+        let userToSet = null;
+        switch (Number(Constants.expoConfig.extra.USER)) {
+          case 1:
+            // DEMO: cliente
+            userToSet = { id: 1, role: "cliente" };
+            break;
+          case 2:
+            // DEMO: doctor
+            userToSet = { id: 2, role: "doctor" };
+            break;
+          default:
+            // DEMO: login
+            userToSet = null;
+            break;
+        }
 
         setUser(userToSet);
         setIsAuthenticated(!!userToSet);
