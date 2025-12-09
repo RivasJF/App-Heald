8f88e7d5388131879b682368fb7a72706fbee233
UsPortatil <jonathariflo@gmail.com>
2025-12-08 21:29:21 -0600

    refactor: better structure doctor

--- app/(app)/(doctor)/_layout.js
+++ app/(app)/(doctor)/_layout.js
@@ -1,11 +1,30 @@
 import { Tabs } from "expo-router";
+import { FontAwesome } from '@expo/vector-icons';
 
-export default function Layout() {
+export default function DoctorLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" options={{ title: "Panel" }} />
-      <Tabs.Screen name="back" options={{ title: "Back" }} />
-      <Tabs.Screen name="login" options={{ title: "Login" }} />
+    <Tabs screenOptions={{ headerShown: false }}>
+      <Tabs.Screen
+        name="hoy"
+        options={{
+          title: "Hoy",
+          tabBarIcon: ({ color, size }) => <FontAwesome name="calendar-check-o" size={size} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="agenda"
+        options={{
+          title: "Agenda",
+          tabBarIcon: ({ color, size }) => <FontAwesome name="calendar" size={size} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="perfil"
+        options={{
+          title: "Perfil",
+          tabBarIcon: ({ color, size }) => <FontAwesome name="user-md" size={size} color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
